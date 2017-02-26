3f14498957e4a11897a68b6a408ecef290073372
denuko <denuko@192.168.1.68>
2017-02-26 22:17:38 +0200

    Cat model

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,13 +1,10 @@
-var ViewModel = function () {
+var Cat = function () {
+    
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
     this.nicknames = ko.observableArray(['Fluffy', 'Mr Wiskers', 'Mitsi', 'Kiki', 'Neko']);
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    };
-
     this.level = ko.computed(function () {
         var level;
         switch (true) {
@@ -36,6 +33,14 @@ var ViewModel = function () {
 
         return level;
     }, this);
-}
+};
+
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat());
+    
+    this.incrementCounter = function () {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+};
 
 ko.applyBindings(new ViewModel());
